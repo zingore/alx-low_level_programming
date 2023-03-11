9c03c90a067690a0aaaa454c58b9655e2b38c62b
zingore <ziadnkiri2@gmail.com>
2023-03-11 14:07:30 -0800

    add.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints its name, followed by a new line.
  * @argc: argument count
@@ -10,7 +9,6 @@
 int main(int argc, char **argv)
 {
 	(void)argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
